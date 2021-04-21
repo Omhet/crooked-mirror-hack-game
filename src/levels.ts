7067d9768ef1dbe3b30a95cd66b3e58acdab26e5
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-04-21 11:05:34 +0300

    add images

--- src/levels.ts
+++ src/levels.ts
@@ -1,13 +1,12 @@
 import img0 from "./images/img-0.jpg";
-import img1 from "./images/img-1.png";
+import img1 from "./images/img-1.jpg";
 import { Level } from "./types";
 
 export const levels: Level[] = [
   {
     img: img0,
     initialSteps: [
-      { imgOptions: { flipY: true }, posOptions: { gridSize: 2, row: 1 } },
-      { imgOptions: { flipY: true }, posOptions: { gridSize: 3, row: 1 } },
+      { imgOptions: { flipX: true }, posOptions: { gridSize: 2, row: 1 } },
     ],
     redundantSteps: [{ imgOptions: { flipX: true } }],
     tries: 3,
