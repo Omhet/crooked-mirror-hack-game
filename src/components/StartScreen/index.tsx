6d58bc15b72975c0492f9c5e0845d4a474e2ab09
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-04-22 23:45:25 +0300

    add story

--- src/components/StartScreen/index.tsx
+++ src/components/StartScreen/index.tsx
@@ -1,13 +1,35 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import HackerIcon from "../../images/hacker.svg";
 import { startGameAction } from "../../store/gameState";
 import { Button } from "../Button";
 import s from "./index.module.css";
-import { setUserNameAction } from "../../store/user";
+import { setUserNameAction, userStore } from "../../store/user";
+import { useStore } from "effector-react";
+
+const getStoryText = (name: string) => `
+Ok, yes.
+But firstly, here is a bit of history...
+
+In the year 2048 the government banned all internet memes
+People are struggling
+The Inthernet needs its new hero who will hack the system and restore all the memes
+
+It might be even you ${name}
+`;
 
 export const StartScreen: FC = () => {
-  return (
-    <div className={s.main}>
+  const [showStory, setShowStory] = useState(false);
+
+  const { name } = useStore(userStore);
+  const story = (
+    <>
+      <p className={s.storyText}>{getStoryText(name)}</p>
+      <Button onClick={startGameAction}>Ok. I'm ready</Button>
+    </>
+  );
+
+  const menu = (
+    <>
       <h1>Crooked Mirror Hacker</h1>
       <HackerIcon />
       <input
@@ -17,7 +39,9 @@ export const StartScreen: FC = () => {
         onChange={(e) => setUserNameAction(e.target.value)}
         placeholder="Enter your hacker name here"
       />
-      <Button onClick={startGameAction}>START</Button>
-    </div>
+      <Button onClick={() => setShowStory(true)}>LET'S HACK</Button>
+    </>
   );
+
+  return <div className={s.main}>{showStory ? story : menu}</div>;
 };
