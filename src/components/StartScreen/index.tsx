f7ac34f9321032ee9b351d8a4e870dd4081e31a3
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-04-22 23:22:37 +0300

    input user name

--- src/components/StartScreen/index.tsx
+++ src/components/StartScreen/index.tsx
@@ -3,13 +3,21 @@ import HackerIcon from "../../images/hacker.svg";
 import { startGameAction } from "../../store/gameState";
 import { Button } from "../Button";
 import s from "./index.module.css";
+import { setUserNameAction } from "../../store/user";
 
 export const StartScreen: FC = () => {
   return (
     <div className={s.main}>
+      <h1>Crooked Mirror Hacker</h1>
       <HackerIcon />
-      <h2>Crooked Mirror Hacker</h2>
-      <Button onClick={startGameAction}>START GAME</Button>
+      <input
+        autoFocus
+        className={s.input}
+        type="text"
+        onChange={(e) => setUserNameAction(e.target.value)}
+        placeholder="Enter your hacker name here"
+      />
+      <Button onClick={startGameAction}>START</Button>
     </div>
   );
 };
