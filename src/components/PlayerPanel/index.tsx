f9f50c95b36fba9a26f37b8ec10f2f5cd3bd2fc9
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-04-23 21:55:32 +0300

    add radio

--- src/components/PlayerPanel/index.tsx
+++ src/components/PlayerPanel/index.tsx
@@ -1,15 +1,67 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Panel } from "../Panel/Panel";
 import s from "./index.module.css";
+import { RadioBrowserApi } from "radio-browser-api";
+import { Button } from "../Button";
+import PrevIcon from "../../images/prev.svg";
+import NextIcon from "../../images/next.svg";
 
-type Props = {
-  isClicked?: boolean;
+const api = new RadioBrowserApi(fetch.bind(window), "Crooked Mirror Game");
+
+const getStations = async () => {
+  const stations = await api.searchStations({
+    language: "english",
+    tag: "techno",
+    tagExact: true,
+    limit: 5,
+  });
+  return stations.map(({ urlResolved }) => urlResolved);
 };
 
-export const PlayerPanel: FC<Props> = ({ isClicked }) => {
+type Props = {};
+
+export const PlayerPanel: FC<Props> = ({}) => {
+  const [stations, setStations] = useState<string[]>([]);
+  const [stationIndex, setStationIndex] = useState<number>(0);
+
+  useEffect(() => {
+    getStations().then(setStations);
+  }, []);
+
+  console.log(stations);
+
   return (
     <div className={s.main}>
-      <Panel title="PLAYER">Hello</Panel>
+      <Panel title="PLAYER">
+        {stations.length > 0 && (
+          <audio
+            key={stationIndex}
+            autoPlay
+            controls
+            src={stations[stationIndex]}
+          ></audio>
+        )}
+        <div className={s.controls}>
+          <Button
+            onClick={() =>
+              setStationIndex((prev) =>
+                prev === 0 ? stations.length - 1 : prev - 1
+              )
+            }
+          >
+            <PrevIcon />
+          </Button>
+          <Button
+            onClick={() =>
+              setStationIndex((prev) =>
+                prev === stations.length - 1 ? 0 : prev + 1
+              )
+            }
+          >
+            <NextIcon />
+          </Button>
+        </div>
+      </Panel>
     </div>
   );
 };
