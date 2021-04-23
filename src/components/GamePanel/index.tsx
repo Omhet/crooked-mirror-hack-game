00a91510c3b9afd9a93ae9795ce52b19a73cb191
Vladimir Ivanov <omhetehmo@yandex.ru>
2021-04-23 09:53:12 +0300

    overlay

--- src/components/GamePanel/index.tsx
+++ src/components/GamePanel/index.tsx
@@ -1,5 +1,6 @@
 import { useStore } from "effector-react";
 import React, { FC } from "react";
+import { chatStore } from "../../store/chat";
 import { imageStore } from "../../store/image";
 import { levelStore } from "../../store/level";
 import { Canvas } from "../Canvas/Canvas";
@@ -12,22 +13,19 @@ type Props = {};
 export const GamePanel: FC<Props> = ({}) => {
   const { isLevelLoading, level } = useStore(levelStore);
   const { showOriginal } = useStore(imageStore);
+  const { isBusy } = useStore(chatStore);
 
   return (
     <div className={s.main}>
       <Panel contentClassName={s.content} title="CROOKED MIRROR CRACKER">
-        {isLevelLoading ? (
-          <div>Loading...</div>
-        ) : (
-          <>
-            <div className={s.imageArea}>
-              <Canvas />
-              {showOriginal && <img className={s.original} src={level.img} />}
-            </div>
+        <div className={s.imageArea}>
+          <Canvas />
+          {showOriginal && <img className={s.original} src={level.img} />}
+        </div>
 
-            <UpdateButtons />
-          </>
-        )}
+        <UpdateButtons />
+
+        {isBusy && <div className={s.overlay}>NO_DATA</div>}
       </Panel>
     </div>
   );
