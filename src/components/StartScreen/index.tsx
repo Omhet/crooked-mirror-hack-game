a740de62a273963f5f574fadfbd49e80073a8457
Vladimir Ivanov <vla.a.ivanov@tinkoff.ru>
2021-04-25 19:10:59 +0500

    start story

--- src/components/StartScreen/index.tsx
+++ src/components/StartScreen/index.tsx
@@ -7,14 +7,17 @@ import { setUserNameAction, userStore } from "../../store/user";
 import { useStore } from "effector-react";
 
 const getStoryText = (name: string) => `
-Ok, yes.
-But firstly, here is a bit of history...
+The year is 2077. People are completely lazy and instead of working, they hang out all day long on the Internet and watch memes. The governments of all countries have joined together to put an end to this. To make people work, they decided to ban all online entertainment, including memes.
 
-In the year 2048 the government banned all internet memes
-People are struggling
-The Inthernet needs its new hero who will hack the system and restore all the memes
+Governments have hired the best people to create a global firewall that encrypts all memes on the web. And it worked. They created a powerful software called "Crooked Mirror". Now all memes are crooked and people are forced to work, because there is nothing else to do.
 
-It might be even you ${name}
+But even in such a dark hour, there were people who were ready to stand up for freedom. The hacker group "Looking Glass" is our last chance for salvation.
+
+They managed to recreate the algorithm of the "Crooked Mirror" and create software that allows you to pick up the decryption. 
+
+You, ${name}, are one of them. You shall help them to decrypt those memes and save the Internet.
+
+V1v4 L4 V1D4
 `;
 
 export const StartScreen: FC = () => {
