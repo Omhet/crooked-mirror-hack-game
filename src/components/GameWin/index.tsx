f3810a10afc9d9d767ac73d2b3617dd6f6965b92
Vladimir Ivanov <vla.a.ivanov@tinkoff.ru>
2021-04-25 21:17:27 +0500

    final choice story

--- src/components/GameWin/index.tsx
+++ src/components/GameWin/index.tsx
@@ -1,10 +1,11 @@
 import React, { FC, useState } from "react";
-import { stopGameAction, gameStateStore } from "../../store/gameState";
+import { stopGameAction } from "../../store/gameState";
 import { ChatMessageFrom, GameFinalChoice } from "../../types";
+import { MessageAuthorNameMap } from "../../utils";
 import { Button } from "../Button";
 import s from "./index.module.css";
 import { useStore } from "effector-react";
-import { MessageAuthorNameMap } from "../../utils";
+import { userStore } from "../../store/user";
 
 type Props = {};
 
@@ -18,24 +19,69 @@ Doubts crept into your head. What if ${friend} gets the same message and turns m
 Halfway through, you stop and decide that it's time to choose for yourself.
 `;
 
-const getFinalText = (choice: GameFinalChoice) => `
-It's the end text.
+const getFinalText = (
+  choice: GameFinalChoice,
+  friend: string,
+  user: string
+) => `
+${
+  choice === GameFinalChoice.Friend
+    ? getFriendChoiceText(friend, user)
+    : getPoliceChoiceText(friend, user)
+}
+`;
 
-The text here will depend on the final user's choice
+const getPoliceChoiceText = (friend: string, user: string) => `
+You send a message to the cops with the coordinates of the meeting.
+"He's a nice guy, but I don't feel like I can trust him." - you thought.
 
-User choosed ${choice === GameFinalChoice.Friend ? "friend" : "police"}
+Agents were already at the meeting place. 
+They put ${friend} in a black car and drove away.
+
+Agent Smith came out of nowhere and told you:
+- Hi, ${user} It's been a pleasure doing business with you. Now, get in the car, we have another offer for you.
+
+You were "offered" to work for the government and you couldn't help but agree.
+
+Later, you learned from the news that ${friend} was murdered in prison.
+This story was made public and there was a lot of hype. People began to take to the streets to protest, and ${friend} was recognized as a national hero.
+
+You have been declared a vile traitor by the public, and you are not welcome even on Twitter, let alone on Reddit. Everyone believes that you wanted to prevent ${friend} and "Looking Glass" from saving memes.
+
+This story has inspired millions of people to take action. "Looking Glass" has not ceased to exist. Their software appeared on the darknet and free hackers joined together to continue the ${user}'s glorious cause of ${friend}. They feel they are strong enough now to finally free the Internet.
+`;
+
+const getFriendChoiceText = (friend: string, user: string) => `
+You've arrived at the meeting place, but didn't see ${friend} anywhere.
+- "It's not like him. He's very punctual"
+
+Suddenly, you felt a sharp pain. 
+Your hands were twisted.
+AGENTS! Fuck.
+
+They put you in a black car and drove away.
+
+The next day, in the jail you learned that ${friend} turned you in. He was "offered" to work for the government and he couldn't help but agree.
+
+A few days later you were murdered by the government stooges...
+
+This story was made public and there was a lot of hype. People began to take to the streets to protest, and you were recognized as a national hero.
+
+${friend} has been declared a vile traitor by the public, and he is not welcome even on Twitter, let alone on Reddit. Everyone believes that he wanted to prevent you and "Looking Glass" from saving memes.
+
+This story has inspired millions of people to take action. "Looking Glass" has not ceased to exist. Their software appeared on the darknet and free hackers joined together to continue the ${user}'s glorious cause. They feel they are strong enough now to finally free the Internet.
 `;
 
 export const GameWin: FC<Props> = ({}) => {
   const [choice, setChoice] = useState<GameFinalChoice | undefined>();
+  const { name: user } = useStore(userStore);
+  const friend = MessageAuthorNameMap[ChatMessageFrom.Friend];
 
   return (
     <div className={s.main}>
       {choice === undefined ? (
         <>
-          <p className={s.storyText}>
-            {getPrefinalText(MessageAuthorNameMap[ChatMessageFrom.Friend])}
-          </p>
+          <p className={s.storyText}>{getPrefinalText(friend)}</p>
 
           <div className={s.finalChoice}>
             <Button
@@ -56,9 +102,11 @@ export const GameWin: FC<Props> = ({}) => {
         </>
       ) : (
         <>
-          <h2>The End</h2>
-          <p className={s.storyText}>{getFinalText(choice)}</p>
-          <Button onClick={stopGameAction}>back to the menu</Button>
+          <h2>Consequences</h2>
+          <p className={s.storyText}>{getFinalText(choice, friend, user)}</p>
+          <Button className={s.backButton} onClick={stopGameAction}>
+            back to the menu
+          </Button>
         </>
       )}
     </div>
